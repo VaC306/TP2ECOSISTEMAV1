edb4e3fbc2e367502e2987339a758041bc6b6d75
Juan diego <juandiea@ucm.es>
2024-02-05 16:50:10 +0100

    DefaultRegion entero

--- TP2ECOSISTEMAV1/src/simulator/model/DefaultRegion.java
+++ TP2ECOSISTEMAV1/src/simulator/model/DefaultRegion.java
@@ -9,11 +9,18 @@ public class DefaultRegion extends Region{
 
 	@Override
 	public double get_food(Animal a, double dt) {
+		int n=0;
+		for(Animal b: getAnimals()) {
+			if(b.get_diet()== Diet.HERBIVORE) {
+				n++;
+			}
+		}
+		
 		if(a.get_diet() == Diet.CARNIVORE)
 			return 0.0;
 		else
-			//return 60.0*Math.exp(-Math.max(0,n-5.0)*2.0)*dt;
-			return 0;
+			return 60.0*Math.exp(-Math.max(0,n-5.0)*2.0)*dt;
+			
 	}
 	
 	
