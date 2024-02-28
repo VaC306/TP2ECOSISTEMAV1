ab5a05a11877d6cb66df55b5cc1181fe88d9f03b
VaC306 <rishipursnani@gmail.com>
2024-02-28 11:46:07 +0100

    cambios en sheepbuilder

--- TP2ECOSISTEMAV1/src/simulator/factories/SheepBuilder.java
+++ TP2ECOSISTEMAV1/src/simulator/factories/SheepBuilder.java
@@ -1,7 +1,9 @@
 package simulator.factories;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import simulator.misc.Vector2D;
 import simulator.model.Animal;
 import simulator.model.SelectionStrategy;
 import simulator.model.Sheep;
@@ -22,13 +24,42 @@ public class SheepBuilder extends Builder<Animal>{
 
 	@Override
 	protected Animal create_instance(JSONObject data) {
+		
 		//valid data
 		if(data == null)
 			throw new IllegalArgumentException();
-				
-		if(data.has()))
 			
+		SelectionStrategy _select_strat;
 		
+		if(!data.has("mate_strategy"))
+		{
+			_select_strat = _selection_strategy_factory.create_instance(data); //ver como selectfirst
+		}
+		
+		if(!data.has("danger_strategy"))
+		{
+			_select_strat = _selection_strategy_factory.create_instance(data); //ver como selectfirst
+		}
+		
+		Vector2D _pos = new Vector2D();
+		JSONArray _x_range = data.getJSONArray("x_range");
+		JSONArray _y_range = data.getJSONArray("y_range");
+		
+		//se comrpueba q sean 2d
+		if(_x_range.length()!=2 || _y_range.length()!=2 ) {
+					
+			throw new IllegalArgumentException();
+		}
+		
+		if(!data.has("pos"))
+		{
+			_pos = null;
+		}
+		else
+		{
+			assert(_pos.getX() >= _x_range.getDouble(0) && _pos.getX() <= _x_range.getDouble(1));
+			assert(_pos.getY() >= _y_range.getDouble(0) && _pos.getY() <= _y_range.getDouble(1));
+		}
 		
 		return null;
 	}
